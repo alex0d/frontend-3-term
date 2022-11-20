e86cd8bc4875ca41074b36fb82d38a85225931f0
alex0d <alexey.d707@gmail.com>
2022-11-20 03:52:26 +0300

    Drawing likes

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -14,5 +14,38 @@ $(function() {
     $(".heart").each(function(index, element) {
         $(element).data("is_like_pressed", false);
         $(element).on("click", $like_listener);
-    })
+    });
+
+    const currentMousePos = {x: -1, y: -1};
+    $(document).mousemove(function(event) {
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
+    });
+
+    let $draw_pictures = false;
+
+    function draw_heart(e = null) {
+        let timeout = 100;
+        let action = function() {
+            if ($draw_pictures) {
+                $("body").append("<div class='black_heart'></div>");
+                let $heart = $(".black_heart").last();
+                $heart.text("❤");
+                $heart.css({
+                    "font-size": "2rem",
+                    "position": "absolute",
+                    "z-index": 1000,
+                    "left": currentMousePos.x,
+                    "top": currentMousePos.y,
+                });
+                setTimeout(action, timeout);
+            }
+        }
+        action();
+    }
+
+    $(".btn-add-image").on("click", function() {
+        $draw_pictures = !$draw_pictures;
+        draw_heart();
+    });
 });
